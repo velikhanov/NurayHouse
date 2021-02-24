c668ece0e07ae641405ec178044fcead5680f0c1
velikhanov <68500204+velikhanov@users.noreply.github.com>
2021-02-24 23:10:02 +0400

    Update configlang.php

--- configs/configlang.php
+++ configs/configlang.php
@@ -6,15 +6,16 @@ $acceptLang = ['ru', 'az', 'en'];
 $lang = in_array($lang, $acceptLang) ? $lang : 'en';
 
 session_start();
-if(!isset($_SESSION['lang']))
+if(!isset($_SESSION['lang'])){
     $_SESSION['lang'] = $lang;
-  else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
-  if ($_GET['lang'] == "en")
+}else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
+  if ($_GET['lang'] == "en"){
     $_SESSION['lang'] = "en";
-  else if ($_GET['lang'] == "az")
+  }else if ($_GET['lang'] == "az"){
     $_SESSION['lang'] = "az";
-  else if ($_GET['lang'] == "ru")
+  }else if ($_GET['lang'] == "ru"){
     $_SESSION['lang'] = "ru";
+  }
 }
 require_once "languages/" . $_SESSION['lang'] . ".php";
 ?>
